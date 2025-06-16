d42ca1e9a5e6150747b1ecb3f1af8d4ca23abe73
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-06-16 12:16:02 -0300

    order-service: ajustes necessários para o padrão saga coreografado

--- order-service/src/main/java/br/com/microservices/choreography/orderservice/core/service/OrderService.java
+++ order-service/src/main/java/br/com/microservices/choreography/orderservice/core/service/OrderService.java
@@ -1,6 +1,5 @@
 package br.com.microservices.choreography.orderservice.core.service;
 
-import br.com.microservices.choreography.orderservice.core.document.Event;
 import br.com.microservices.choreography.orderservice.core.document.Order;
 import br.com.microservices.choreography.orderservice.core.dto.OrderRequest;
 import br.com.microservices.choreography.orderservice.core.producer.SagaProducer;
@@ -34,21 +33,8 @@ public class OrderService {
                 )
                 .build();
         repository.save(order);
-        // talvez de problema @jardelDev
-        producer.sendEvent(jsonUtil.toJson(createPayload(order)));
+        producer.sendEvent(jsonUtil.toJson(eventService.createEvent(order)));
         return order;
-    }
-
-    private Event createPayload(Order order){
-        var event = Event
-                .builder()
-                .orderId(order.getId())
-                .transactionId(order.getTransactionId())
-                .payload(order)
-                .createdAt(LocalDateTime.now())
-                .build();
-        eventService.save(event);
-        return event;
     }
 
 }
