69b217dfcbf80fbbf721ece9607e97e2553f0b1a
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-06-17 12:35:13 -0300

    Criando o SEC(Safa Execution Controller) da coreografia nos microsservicos

--- inventory-service/src/main/java/br/com/microservices/choreography/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/choreography/inventoryservice/core/service/InventoryService.java
@@ -10,6 +10,7 @@ import br.com.microservices.choreography.inventoryservice.core.model.OrderInvent
 import br.com.microservices.choreography.inventoryservice.core.producer.KafkaProducer;
 import br.com.microservices.choreography.inventoryservice.core.repository.InventoryRepository;
 import br.com.microservices.choreography.inventoryservice.core.repository.OrderInventoryRepository;
+import br.com.microservices.choreography.inventoryservice.core.saga.SagaExecutionController;
 import br.com.microservices.choreography.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,8 +27,7 @@ public class InventoryService {
 
     private static final String CURRENT_SOURCE = "INVENTORY_SERVICE";
 
-    private final JsonUtil jsonUtil;
-    private final KafkaProducer producer;
+    private final SagaExecutionController sagaExecutionController;
     private final InventoryRepository inventoryRepository;
     private final OrderInventoryRepository orderInventoryRepository;
 
@@ -41,7 +41,7 @@ public class InventoryService {
             log.error("Error trying to update inventory: ", ex);
             handleFailCurrentNotExecuted(event, ex.getMessage());
         }
-        producer.sendEvent(jsonUtil.toJson(event), "");
+        sagaExecutionController.handleSaga(event);
     }
 
     private void checkCurrentValidation(Event event) {
@@ -120,7 +120,7 @@ public class InventoryService {
         } catch (Exception ex){
             addHistory(event, "Rollback not executed for inventory: ".concat(ex.getMessage()));
         }
-        producer.sendEvent(jsonUtil.toJson(event), "");
+        sagaExecutionController.handleSaga(event);
     }
 
     private void returnInventoryToPreviousValues(Event event) {
--- payment-service/src/main/java/br/com/microservices/choreography/paymentservice/core/service/PaymentService.java
+++ payment-service/src/main/java/br/com/microservices/choreography/paymentservice/core/service/PaymentService.java
@@ -8,6 +8,7 @@ import br.com.microservices.choreography.paymentservice.core.enums.EPaymentStatu
 import br.com.microservices.choreography.paymentservice.core.model.Payment;
 import br.com.microservices.choreography.paymentservice.core.producer.KafkaProducer;
 import br.com.microservices.choreography.paymentservice.core.repository.PaymentRepository;
+import br.com.microservices.choreography.paymentservice.core.saga.SagaExecutionController;
 import br.com.microservices.choreography.paymentservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,9 +27,8 @@ public class PaymentService {
     private static final Double REDUCE_SUM_VALUE = 0.0;
     private static final Double MIN_VALUE_AMOUNT = 0.1;
 
-    private final JsonUtil jsonUtil;
-    private final KafkaProducer producer;
     private final PaymentRepository paymentRepository;
+    private final SagaExecutionController sagaExecutionController;
 
     public void realizePayment(Event event) {
         try {
@@ -42,7 +42,7 @@ public class PaymentService {
             log.error("Error trying to make payment: ", ex);
             handleFailCurrentNotExecuted(event, ex.getMessage());
         }
-        producer.sendEvent(jsonUtil.toJson(event), "");
+        sagaExecutionController.handleSaga(event);
     }
 
     private void checkCurrentValidation(Event event) {
@@ -131,7 +131,7 @@ public class PaymentService {
         } catch (Exception ex) {
             addHistory(event, "Rollback not executed for payment: ".concat(ex.getMessage()));
         }
-        producer.sendEvent(jsonUtil.toJson(event), "");
+        sagaExecutionController.handleSaga(event);
     }
 
     private void changePaymentStatusToRefund(Event event) {
